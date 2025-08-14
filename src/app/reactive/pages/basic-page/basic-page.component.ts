5ea3455cbc9a88bf2c7f7cbc673c7584c3f0b0be
Leonardo Cangialosi <leocangialosi04@gmail.com>
2025-08-14 09:01:38 -0300

    FormGroup & FormControl development

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,12 +1,22 @@
 import { JsonPipe } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-basic-component',
   imports: [
-    JsonPipe
-  ],
+    JsonPipe,
+      ReactiveFormsModule
+],
   templateUrl: './basic-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class BasicPageComponent { }
+export class BasicPageComponent {
+
+  myForm = new FormGroup({
+    name: new FormControl(''),
+    price: new FormControl(0),
+    inStorage: new FormControl(0),
+  });
+
+}
