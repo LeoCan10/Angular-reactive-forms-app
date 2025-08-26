3856f0686fa58259451b6b5cf07ef2b1b9aab23e
Leonardo Cangialosi <leocangialosi04@gmail.com>
2025-08-26 12:16:40 -0300

    Function getFieldError & isValidField

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -15,7 +15,7 @@ export class BasicPageComponent {
 
   fb = inject(FormBuilder);
 
-  myForm = this.fb.group({
+  myForm: FormGroup = this.fb.group({
     name: ['', [Validators.required, Validators.minLength(3)]],
     price: [0, [Validators.required, Validators.min(10)]],
     inStorage: [0, [Validators.required, Validators.min(0)]],
@@ -28,4 +28,27 @@ export class BasicPageComponent {
   //   inStorage: new FormControl(0),
   // });
 
+
+  isValidField(fieldName: string): boolean | null {
+    return !!this.myForm.controls[fieldName].errors;
+  }
+
+  getFieldError(fieldName: string): string | null {
+    if ( !this.myForm.controls[fieldName]) return null;
+
+    const errors = this.myForm.controls[fieldName].errors ?? {};
+
+    for(const key of Object.keys(errors)){
+      switch(key){
+        case 'required':
+          return 'Este campo es requerido';
+        case 'minlength':
+          return `Mínimo ${errors['minlength'].requiredLength} caracteres.`;
+        case 'min':
+          return `El valor mínimo de ${errors['min'].min}.`;
+      }
+    }
+
+      return null;
+  }
 }
